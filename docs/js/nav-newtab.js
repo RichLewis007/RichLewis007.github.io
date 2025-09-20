23317c0689a1d809f116f75c5f5bf834f83efdd8
Rich Lewis <1149213+RichLewis007@users.noreply.github.com>
2025-09-20 00:39:02 -0400

    Update nav-newtab.js

--- docs/js/nav-newtab.js
+++ docs/js/nav-newtab.js
@@ -1,16 +1,1 @@
-// This script sets all external links (links to other domains) to open in a new tab.
-
-function setNewTabForExternalLinks() {
-    const origin = location.origin;
-    document.querySelectorAll('a[href^="http"]').forEach(a => {
-        if (!a.href.startsWith(origin)) {
-            a.setAttribute('target', '_blank');
-            a.setAttribute('rel', 'noopener noreferrer');
-        }
-    });
-}
-
-document.addEventListener('DOMContentLoaded', setNewTabForExternalLinks);
-if (window.document$) {
-    window.document$.subscribe(setNewTabForExternalLinks);
-}
+xxx
